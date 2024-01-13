1e555005603f6b67879b21cac0a1ea2564d78461
Anshuman Sarkar <anshumansarkar2015@gmail.com>
2024-01-13 19:42:03 +0530

    update src/en

---
+++ src/en/nightcomic/src/eu/kanade/tachiyomi/extension/en/nightcomic/NightComic.kt
@@ -0,0 +1,8 @@
+/* ktlint-disable */
+// THIS FILE IS AUTO-GENERATED; DO NOT EDIT
+package eu.kanade.tachiyomi.extension.en.nightcomic
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+
+class NightComic : Madara("Night Comic", "https://www.nightcomic.com", "en")
