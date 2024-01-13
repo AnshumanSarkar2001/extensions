a0e7f3eac7d4e26edf32cb4e40d0e49f030b82f9
Anshuman Sarkar <anshumansarkar2015@gmail.com>
2024-01-13 19:19:20 +0530

    update src/en

---
+++ src/en/cypherscans/src/eu/kanade/tachiyomi/extension/en/cypherscans/CypherScans.kt
@@ -0,0 +1,8 @@
+/* ktlint-disable */
+// THIS FILE IS AUTO-GENERATED; DO NOT EDIT
+package eu.kanade.tachiyomi.extension.en.cypherscans
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+
+class CypherScans : MangaThemesia("Cypher Scans", "https://cypherscans.xyz", "en")
