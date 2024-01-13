8a282a10c4951c82093a8130fc8d6fe7dbd14a05
Anshuman Sarkar <anshumansarkar2015@gmail.com>
2024-01-13 19:13:48 +0530

    update src /en

---
+++ src/en/arcanescans/src/eu/kanade/tachiyomi/extension/en/arcanescans/Arcanescans.kt
@@ -0,0 +1,8 @@
+/* ktlint-disable */
+// THIS FILE IS AUTO-GENERATED; DO NOT EDIT
+package eu.kanade.tachiyomi.extension.en.arcanescans
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+
+class Arcanescans : Madara("Arcanescans", "https://arcanescans.com", "en")
