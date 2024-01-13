f9f7a138f393d6ef5c47f249cfca580af2cd4404
Anshuman Sarkar <anshumansarkar2015@gmail.com>
2024-01-13 19:26:21 +0530

    update src/en

---
+++ src/en/isekaiscaneu/src/eu/kanade/tachiyomi/extension/en/isekaiscaneu/IsekaiScanTo.kt
@@ -0,0 +1,8 @@
+/* ktlint-disable */
+// THIS FILE IS AUTO-GENERATED; DO NOT EDIT
+package eu.kanade.tachiyomi.extension.en.isekaiscaneu
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+
+class IsekaiScanTo : Madara("IsekaiScan.to (unoriginal)", "https://m.isekaiscan.to", "en")
