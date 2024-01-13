427f98bb21b76664d4f377d399c228689f496495
Anshuman Sarkar <anshumansarkar2015@gmail.com>
2024-01-13 19:22:22 +0530

    update src/en

---
+++ src/en/freakscans/src/eu/kanade/tachiyomi/extension/en/freakscans/FreakScans.kt
@@ -0,0 +1,8 @@
+/* ktlint-disable */
+// THIS FILE IS AUTO-GENERATED; DO NOT EDIT
+package eu.kanade.tachiyomi.extension.en.freakscans
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+
+class FreakScans : MangaThemesia("Freak Scans", "https://freakscans.com", "en")
---
+++ src/en/galaxydegenscans/src/eu/kanade/tachiyomi/extension/en/galaxydegenscans/GalaxyDegenScans.kt
@@ -0,0 +1,8 @@
+/* ktlint-disable */
+// THIS FILE IS AUTO-GENERATED; DO NOT EDIT
+package eu.kanade.tachiyomi.extension.en.galaxydegenscans
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+
+class GalaxyDegenScans : Madara("GalaxyDegenScans", "https://gdscans.com", "en")
