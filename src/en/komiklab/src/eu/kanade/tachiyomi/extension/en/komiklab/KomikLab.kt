8cc3f5618501dfe6c2ff538434af5e5e75c6e723
Anshuman Sarkar <anshumansarkar2015@gmail.com>
2024-01-13 19:31:34 +0530

    update src/en

---
+++ src/en/komiklab/src/eu/kanade/tachiyomi/extension/en/komiklab/KomikLab.kt
@@ -0,0 +1,8 @@
+/* ktlint-disable */
+// THIS FILE IS AUTO-GENERATED; DO NOT EDIT
+package eu.kanade.tachiyomi.extension.en.komiklab
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+
+class KomikLab : MangaThemesia("Komik Lab", "https://komiklab.com", "en")
