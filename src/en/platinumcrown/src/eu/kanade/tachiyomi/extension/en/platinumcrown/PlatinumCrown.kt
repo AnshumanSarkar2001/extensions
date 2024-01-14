c045d863e1c0e96e92e3a66d8e8373b7b2b9ba67
Anshuman Sarkar <anshumansarkar2015@gmail.com>
2024-01-14 18:36:19 +0530

    update src/en

---
+++ src/en/platinumcrown/src/eu/kanade/tachiyomi/extension/en/platinumcrown/PlatinumCrown.kt
@@ -0,0 +1,8 @@
+/* ktlint-disable */
+// THIS FILE IS AUTO-GENERATED; DO NOT EDIT
+package eu.kanade.tachiyomi.extension.en.platinumcrown
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+
+class PlatinumCrown : Madara("Platinum Crown", "https://platinumscans.com", "en")
