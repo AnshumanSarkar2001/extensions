96678b98665110424f155bff104809335068a6b4
Anshuman Sarkar <anshumansarkar2015@gmail.com>
2024-01-15 14:57:15 +0530

    update src/en

--- src/en/webdexscans/src/eu/kanade/tachiyomi/extension/en/webdexscans/WebdexScans.kt
+++ src/en/webdexscans/src/eu/kanade/tachiyomi/extension/en/webdexscans/WebdexScans.kt
@@ -5,39 +5,36 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.Page
 import okhttp3.Request
-import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
-import org.jsoup.select.Elements
 import java.nio.charset.StandardCharsets
 import java.util.Base64
 
 class WebdexScans : Madara("Webdex Scans", "https://webdexscans.com", "en") {
     override val useNewChapterEndpoint = true
-    override val mangaDetailsSelectorStatus = "div.summary-heading:contains(Status) + div.summary-content"
+    override val mangaDetailsSelectorStatus =
+        "div.summary-heading:contains(Status) + div.summary-content"
+    override val mangaSubString = "series"
 
     override fun searchPage(page: Int): String = if (page == 1) "" else "page/$page/"
 
     @SuppressLint("NewApi")
-    override fun pageListParse(document: Document): List<Page> {
-        super.countViews(document)
-
-        val imgList: Elements = document.select("img.wp-manga-chapter-img")
-        val mutablePageList = mutableListOf<Page>()
-
-        for (img: Element in imgList) {
-            val data = img.attr("src")
-            val decodedBytes = Base64.getDecoder().decode(data.substringAfter(";base64,"))
-            val imgUrl = String(decodedBytes, StandardCharsets.UTF_8) as? String
-            val newData = imgUrl?.substringAfter("data-u=\"")?.substringBefore("\"")
-                ?.replace("%3A", ":")
-                ?.replace("%2F", "/")
-            val page = Page(imgList.indexOf(img), imageUrl = newData)
-            mutablePageList.add(page)
-        }
-        return mutablePageList.toList()
+    override fun imageFromElement(element: Element): String? {
+        val data = element.attr("src")
+        val decodedBytes = Base64.getDecoder().decode(data.substringAfter(";base64,"))
+        val imgUrl = String(decodedBytes, StandardCharsets.UTF_8) as? String
+        val newData = imgUrl?.substringAfter("data-u=\"")?.substringBefore("\"")
+            ?.replace("%3A", ":")
+            ?.replace("%2F", "/")
+        return newData
     }
 
     override fun imageRequest(page: Page): Request {
-        return GET(page.imageUrl!!, headers.newBuilder().set("Content-Type", "image/svg+xml").build())
+        if (page.url.isEmpty()) {
+            return GET(
+                page.imageUrl!!,
+                headers.newBuilder().set("Content-Type", "image/svg+xml").build(),
+            )
+        }
+        return super.imageRequest(page)
     }
 }
