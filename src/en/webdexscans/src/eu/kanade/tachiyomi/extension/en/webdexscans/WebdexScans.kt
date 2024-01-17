e380f0282b68d7ab643ab2810cef922edd2f2c67
Anshuman Sarkar <anshumansarkar2015@gmail.com>
2024-01-17 22:28:09 +0530

    Change from base64 decryption to normal

--- src/en/webdexscans/src/eu/kanade/tachiyomi/extension/en/webdexscans/WebdexScans.kt
+++ src/en/webdexscans/src/eu/kanade/tachiyomi/extension/en/webdexscans/WebdexScans.kt
@@ -1,13 +1,6 @@
 package eu.kanade.tachiyomi.extension.en.webdexscans
 
-import android.annotation.SuppressLint
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.source.model.Page
-import okhttp3.Request
-import org.jsoup.nodes.Element
-import java.nio.charset.StandardCharsets
-import java.util.Base64
 
 class WebdexScans : Madara("Webdex Scans", "https://webdexscans.com", "en") {
     override val useNewChapterEndpoint = true
@@ -17,24 +10,24 @@ class WebdexScans : Madara("Webdex Scans", "https://webdexscans.com", "en") {
 
     override fun searchPage(page: Int): String = if (page == 1) "" else "page/$page/"
 
-    @SuppressLint("NewApi")
-    override fun imageFromElement(element: Element): String? {
-        val data = element.attr("src")
-        val decodedBytes = Base64.getDecoder().decode(data.substringAfter(";base64,"))
-        val imgUrl = String(decodedBytes, StandardCharsets.UTF_8) as? String
-        val newData = imgUrl?.substringAfter("data-u=\"")?.substringBefore("\"")
-            ?.replace("%3A", ":")
-            ?.replace("%2F", "/")
-        return newData
-    }
+//    @SuppressLint("NewApi")
+//    override fun imageFromElement(element: Element): String? {
+//        val data = element.attr("src")
+//        val decodedBytes = Base64.getDecoder().decode(data.substringAfter(";base64,"))
+//        val imgUrl = String(decodedBytes, StandardCharsets.UTF_8) as? String
+//        val newData = imgUrl?.substringAfter("data-u=\"")?.substringBefore("\"")
+//            ?.replace("%3A", ":")
+//            ?.replace("%2F", "/")
+//        return newData
+//    }
 
-    override fun imageRequest(page: Page): Request {
-        if (page.url.isEmpty()) {
-            return GET(
-                page.imageUrl!!,
-                headers.newBuilder().set("Content-Type", "image/svg+xml").build(),
-            )
-        }
-        return super.imageRequest(page)
-    }
+//    override fun imageRequest(page: Page): Request {
+//        if (page.url.isEmpty()) {
+//            return GET(
+//                page.imageUrl!!,
+//                headers.newBuilder().set("Content-Type", "image/svg+xml").build(),
+//            )
+//        }
+//        return super.imageRequest(page)
+//    }
 }
